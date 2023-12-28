64d9179040506052aa0ae6230aeeef130a750d34
Nyisztor Zoltán <znyisztor77@gmail.com>
2023-12-28 14:44:45 +0100

    Add project files.

---
+++ Autok_Osztalyok/Auto.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autok_Osztalyok
+{
+    internal class Auto
+    {
+        string marka;
+        string tipus;
+        int kivitel;
+        int evjarat;
+
+        public Auto(string marka, string tipus, int kivitel, int evjarat)
+        {
+            this.marka = marka;
+            this.tipus = tipus;
+            this.kivitel = kivitel;
+            this.evjarat = evjarat;
+        }
+
+        public string GetMarka()
+        {
+            return marka;
+        }
+        public string GetTipus()
+        {
+            return tipus;
+        }
+        public string GetKivitel()
+        {
+            switch (kivitel)
+            {
+                case 0: return "Nem személygépjármű";
+                case 1: return "Sedan";
+                case 2: return "SUV";
+                case 3: return "Hatchback";
+                case 4: return "Kombi";
+                case 5: return "Terepjáró";
+                case 6: return "Egyterű";
+            }
+            return "Ismeretlen";
+        }
+        public int GetEvjarat()
+        {
+            return evjarat;
+        }
+        public override string ToString()
+        {
+            return $"{marka};{tipus};{kivitel};{evjarat}";
+        }
+    }
+}
---
+++ Autok_Osztalyok/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autok_Osztalyok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Auto mercedes = new Auto("Mercedes", "S class", 1, 2018);
+            Auto audi = new Auto("Audi", "R line", 2, 2023);
+        }
+    }
+}
